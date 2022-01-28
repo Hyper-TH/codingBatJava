04f90156dabc5f913cc98a2be5810dd913066de6
Hyper-TH <twila.shayne.habab@outlook.com>
2022-01-28 01:27:02 +0000

    Added program descriptions

--- codingBatJava/warmUp1/makes10/Main.java
+++ codingBatJava/warmUp1/makes10/Main.java
@@ -1,4 +1,11 @@
-package makes10;
+/*  
+    Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+    makes10(9, 10) → true
+    makes10(9, 9) → false
+    makes10(1, 9) → true
+*/
+package codingBatJava.warmUp1.makes10;
 
 public class Main {
 
--- codingBatJava/warmUp1/sleepIn/Main.java
+++ codingBatJava/warmUp1/sleepIn/Main.java
@@ -1,18 +1,19 @@
-package sleepIn;
+package codingBatJava.warmUp1.sleepIn;
 /*
-The parameter weekday is true if it is a weekday, 
-and the parameter vacation is true if we are on vacation. 
-We sleep in if it is not a weekday or we're on vacation. 
-Return true if we sleep in.
+    The parameter weekday is true if it is a weekday, 
+    and the parameter vacation is true if we are on vacation. 
+    We sleep in if it is not a weekday or we're on vacation. 
+    Return true if we sleep in.
 
-sleepIn(false, false) → true
-sleepIn(true, false) → false
-sleepIn(false, true) → true
+    sleepIn(false, false) → true
+    sleepIn(true, false) → false
+    sleepIn(false, true) → true
 */
 public class Main {
-
+    
     public boolean sleepIn(boolean weekday, boolean vacation) {
         // Return true if its not weekday and its vacation
         return (!weekday || vacation);
     }
 }
+
